1378dae4418a95e24dcaef432790dd9c8bf77985
mumuumu <bylas77@naver.com>
2023-11-01 16:35:17 +0900

    엔딩, 게임오버

---
+++ Assets/Scripts/TypingEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TypingEffect : MonoBehaviour
+{
+    public TMP_Text tx;
+    private string tmp_text = "그렇게 나는 무사히 졸업시험을 통과했다";
+    void Start()
+    {
+        StartCoroutine(_typing());
+    }
+
+    IEnumerator _typing()
+    {
+
+        for (int i = 0; i <= tmp_text.Length; i++)
+        {
+            tx.text = tmp_text.Substring(0, i); // text.Substring(시작위치, 개수)
+
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
+}
